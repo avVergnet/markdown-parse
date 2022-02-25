2588eba7309523d64d5974541134c213f8a2f548
avVergnet <avergnet@ucsd.edu>
2022-02-24 20:25:05 -0800

    da

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -59,4 +59,38 @@ public class MarkdownParseTest{
 
         assertEquals(expectedOutput, links);
     }
+
+
+    @Test
+    public void testSnipet1() throws IOException{
+        Path fileName = Path.of("snip1.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        List<String> expectedOutput = List.of("%60google.com", "google.com",  "ucsd.edu");
+
+        assertEquals(expectedOutput, links);    
+    }
+
+    @Test
+    public void testSnipet2() throws IOException{
+        Path fileName = Path.of("snip2.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        List<String> expectedOutput = List.of("a.com", "a.com(())", "example.com");
+
+        assertEquals(expectedOutput, links);    
+    }
+
+    @Test
+    public void testSnipet3() throws IOException{
+        Path fileName = Path.of("snip3.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        List<String> expectedOutput = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", "https://cse.ucsd.edu/");
+
+        assertEquals(expectedOutput, links);    
+    }
 }
